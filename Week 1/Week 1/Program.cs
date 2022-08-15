f22da83961c29f6338e33de125a243c82ecdc250
D3V3L0P3 <93241606+D3V3L0P3@users.noreply.github.com>
2022-08-15 18:25:32 +1000

    Add files via upload

---
+++ Week 1/Week 1/Program.cs
@@ -0,0 +1,1 @@
+﻿// week 1 lectutes
---
+++ Week 2/CAB201 WK2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AgeDisplay
+{
+    class AgeDisplay
+
+    {
+        /// <summary>
+        /// </summary>
+        /// <param name = 'args' \></param>
+        static void Main(string[] args)
+        {
+            int age = 17;
+
+            if (age >= 65)
+            {
+                Console.WriteLine("Enjoy your retirement");
+            }
+
+            else if (age >= 18)
+            {
+                Console.WriteLine("would your like an alcoholic beverage");
+            }
+
+            else if (age >= 16)
+            {
+                Console.WriteLine("You're old enough to drive");
+            }
+
+            else
+            {
+                Console.WriteLine("You're too young")
+            }
+        }
+    }
+}
